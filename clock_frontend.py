c1a345dd4b64957e0732996e9996ea385284ee71
marek-kaminski <61633829+marek-kaminski@users.noreply.github.com>
2020-08-20 22:41:57 +0200

    Update clock_frontend.py
    added the option to save the time and working on a feature that will show the progress

--- clock_frontend.py
+++ clock_frontend.py
@@ -2,6 +2,8 @@
 from tkinter import *
 import time
 import csv
+
+
 # from PIL import ImageTk, Image
 
 def Main():
@@ -10,7 +12,7 @@ def Main():
     root = Tk()
     root.title("Stopwatch")
     width = 500
-    height = 500
+    height = 400
 
     # canvas = Canvas(root, width=300, height=160)
     # image = ImageTk.PhotoImage(Image.open("sky_image.png"))
@@ -18,11 +20,12 @@ def Main():
     # canvas.pack()
     # root.mainloop()
 
-
-
     # photo = PhotoImage(file="sky_image.png", )
     # label = Label(root, image=photo, width=900, height=450, bg="black")
     # label.pack()
+
+
+
 
 
     screen_width = root.winfo_screenwidth()
@@ -30,30 +33,58 @@ def Main():
     x = (screen_width / 2) - (width / 2)
     y = (screen_height / 2) - (height / 2)
     root.geometry("%dx%d+%d+%d" % (width, height, x, y))
-    Top = Frame(root, width=600)
+
+    Top = Frame(root, width=600, bg="black")
     Top.pack(side=TOP)
     stopWatch = StopWatch(root)
     stopWatch.pack(side=TOP)
-    Bottom = Frame(root, width=500)
+
+    Middle = Frame(root, width=600, bg="black")
+    Middle.pack(side=TOP)
+
+    Bottom = Frame(root, width=500, bg="black")
     Bottom.pack(side=BOTTOM)
+
+    blank_line3 = Label(Bottom, text="", font=("arial", 18), fg="white", bg="black")
+    blank_line3.pack(side=BOTTOM)
+    blank_line4 = Label(Bottom, text="", font=("arial", 18), fg="white", bg="black")
+    blank_line4.pack(side=TOP)
+
     Start = Button(Bottom, text='Start', command=stopWatch.Start, width=10, height=2)
     Start.pack(side=LEFT)
     Stop = Button(Bottom, text='Stop', command=stopWatch.Stop, width=10, height=2)
     Stop.pack(side=LEFT)
+    Save = Button(Bottom, text='Save', command=stopWatch.Save, width=10, height=2)
+    Save.pack(side=LEFT)
     Reset = Button(Bottom, text='Reset', command=stopWatch.Reset, width=10, height=2)
     Reset.pack(side=LEFT)
-    
+
+    blank_line = Label(Top, text=" ", font=("arial", 18), fg="white", bg="black")
+    blank_line.pack(fill=X)
+
     Title = Label(Top, text="stoper", font=("arial", 18), fg="white", bg="black")
     Title.pack(fill=X)
+    blank_line2 = Label(Middle, text="", font=("arial", 18), fg="white", bg="black")
+    blank_line2.pack(side=TOP)
 
-    # bottom_table = Label(root, text="całkowity czas spędzony na programowaniu   ", font="times 15 bold")
-    # bottom_table.grid(row=3, column=2)
+    Title2 = Label(Middle, text="Całkowity czas spędzony na programowaniu", font=("arial", 18), fg="white", bg="black")
+    Title2.pack(side=TOP)
 
+    with open('programming_time.txt', 'r') as file:
+        reader = csv.reader(file)
+        print(reader)
+        for row in reader:
+            print(row)
+        print(row)
+        minutes_of_coding = float(row[0])//60
+        hours_of_coding = minutes_of_coding//60
+
+    all_programming_time = Label(Middle, text="godziny: " + str(hours_of_coding) + " minuty: " + str(minutes_of_coding),
+                                 font=("arial", 18), fg="white", bg="black")
+    all_programming_time.pack(side=TOP)
 
     root.config(bg="black")
     root.mainloop()
-
-
 
 
 class StopWatch(Frame):
@@ -98,27 +129,49 @@ class StopWatch(Frame):
             self.Updater()
             self.onRunning = 1
 
+    def Save(self):
+
+        self.nextTime = time.time() - self.startTime
+        self.SetTime(self.nextTime)
+
+        with open('programming_time.txt', 'r+') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                all_time = self.nextTime + float(row[0])
+                print(row)
+                print(all_time)
+                f = open('programming_time.txt', "w+")
+                f.write(str(all_time))
+                f.close()
+
+        # try:
+        #     with open('programming_time.txt', "w+") as file:
+        #         reader = csv.reader(file)
+        #         print(reader)
+        #         for row in reader:
+        #             print(row)
+        #             all_time = self.nextTime + reader
+        #             f.write(str(self.nextTime))
+        #             f.close()
+        #
+        #
+        # except FileExistsError:
+        #     f = open('programming_time.txt', "r+")
+        #     f.write(str(self.nextTime))
+        #     f.close()
 
     def Stop(self):
         if self.onRunning:
             self.after_cancel(self.timer)
-            self.nextTime = time.time() - self.startTime
-            self.SetTime(self.nextTime)
             self.onRunning = 0
-            # to poniżej printuje obecny czas, teraz wystarczy zrobić csv który będzie zapisywał czas
-            print(self.nextTime)
-            self.save_csv()
-            with open('programming_time.txt', 'r') as file:
-                reader = csv.reader(file)
-                for row in reader:
-                    print(row)
+
+
+
 
     def Reset(self):
         self.startTime = time.time()
         self.nextTime = 0.0
         self.SetTime(self.nextTime)
-
-
 
 
 if __name__ == '__main__':
